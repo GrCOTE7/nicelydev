151b5e79fa99e0a1815b6ea44d39c47318157349
GrCOTE7 <grcote7@gmail.com>
2025-02-01 15:35:16 +0100

    bases

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP in Docker</title>
+  <style>
+  body {
+    font-family: arial, 'Roboto', sans-serif;
+    color: maroon;
+    font-size: 1.2em;
+    margin: 5px 20px;
+  }
+  </style>
+  <script src="http://localhost:3000/main.js"></script>
+</head>
+
+<body>
+  <?php
+  include_once 'web/content.php';
+  ?>
+</body>
+
+</html>
---
+++ web/content.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '<p>Bonjour ! Je suis PHP de version '. phpversion().'</p>';
+
+//[ ] Nginx + PHP + MySQL dans Docker
+
+?>
+
+<p>(En direct, CLI: php -S "localhost:80")</p>
